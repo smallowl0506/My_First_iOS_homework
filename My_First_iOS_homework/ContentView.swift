ced72aab59f0d6afa5c53b4041e3fb77ede87680
as468579 <as468579@gmail.com>
2020-03-19 18:28:35 +0800

    Initial Commit

---
+++ My_First_iOS_homework/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  My_First_iOS_homework
+//
+//  Created by User05 on 2020/3/19.
+//  Copyright © 2020 steven. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
